b8131c4680097449de7bb3c93a7705dd028c70d4
Andarz <arzjaev@gmail.com>
2024-07-17 17:55:07 +0300

    POM Excersise added

---
+++ POMExcersise/Pages/BasePage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace POMExcersise.Pages
+{
+	public class BasePage
+	{
+		protected IWebDriver driver;
+		protected WebDriverWait wait;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+		protected IWebElement FindElement(By by)
+		{
+			return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(by));
+		}
+    }
+}
